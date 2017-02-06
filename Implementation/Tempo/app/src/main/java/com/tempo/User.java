5942cc85aefa0a7f8f6bf7beb938c493b2652acc
Jessie Smith <smithygirl@gmail.com>
2017-02-06 12:16:19 -0800

    #632222 Adding User Class

---
+++ Implementation/Tempo/app/src/main/java/com/tempo/User.java
@@ -0,0 +1,32 @@
+package com.tempo;
+
+import android.provider.ContactsContract;
+
+/**
+ * Created by Jessie on 2/4/17.
+ */
+
+public class User {
+    private ContactsContract.CommonDataKinds.Email email;
+
+    private void lookUpUser(ContactsContract.CommonDataKinds.Email email) {
+
+    }
+
+    private Group createNewGroup(String groupName) {
+
+    }
+
+    private void deleteGroup(String groupName) {
+
+    }
+
+    private void leaveGroup(String groupName) {
+
+    }
+
+    private void changeLanguage(String Language) {
+
+    }
+
+}
