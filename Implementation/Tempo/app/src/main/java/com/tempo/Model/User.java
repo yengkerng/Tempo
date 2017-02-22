2f9d5eee93d3dd47513db55d666b1bbd3e32ed19
Jessie Smith <smithygirl@gmail.com>
2017-02-22 12:20:51 -0800

    #632222 implementing the methods in the user class

--- Implementation/Tempo/app/src/main/java/com/tempo/Model/User.java
+++ Implementation/Tempo/app/src/main/java/com/tempo/Model/User.java
@@ -1,25 +1,51 @@
 package com.tempo.Model;
-
+import java.util.ArrayList;
 /**
  * Created by Jessie on 2/4/17.
  */
 
 public class User {
+    private String userName;
     private String email;
+    private ArrayList<Group> groups;
+    public User(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
 
     private void lookUpUser(String email) {
 
     }
 
-    private Group createNewGroup(String groupName) {
-        return null;
+    private Group createNewGroup(String groupName, User admin) {
+        ArrayList<User> members = new ArrayList<User>();
+        members.add(admin);
+        Group newGroup = new Group(groupName, admin, members, null);
+        newGroup.name = groupName;
+        admin.groups.add(newGroup);
+        return newGroup;
     }
 
-    private void deleteGroup(String groupName) {
-
+    private boolean deleteGroup(String groupName) {
+        for(int i = 0; i < this.groups.size(); i++) {
+            if(this.groups.get(i).name.equals(groupName)) {
+                this.groups.remove(i);
+                return true;
+            }
+        }
+        //Throw an exception here or somehow display to user that they aren't in a group with that name
+        return false;
     }
 
-    private void leaveGroup(String groupName) {
+    private boolean leaveGroup(String groupName) {
+        for(int i = 0; i < this.groups.size(); i++) {
+            if(this.groups.get(i).name.equals(groupName)) {
+                this.groups.remove(i);
+                return true;
+            }
+        }
+        //Throw an exception here or somehow display to user that they aren't in a group with that name
+        return false;
 
     }
 
