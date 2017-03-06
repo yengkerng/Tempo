9a68f9afd51df1aeb051199144e58afb4695ab26
ajboyd2 <ajboyd@calpoly.edu>
2017-03-05 17:58:25 -0800

    [#642653] Changed the groups attribute to be a List, not an ArrayList. Also removed redundant method.

--- Implementation/Tempo/app/src/main/java/com/tempo/Model/User.java
+++ Implementation/Tempo/app/src/main/java/com/tempo/Model/User.java
@@ -4,45 +4,36 @@ import java.util.List;
 
 /**
  * Created by Jessie on 2/4/17.
+ * Edited by Alex on 3/5/17.
  */
 
 public class User {
     private String userName;
     private String email;
-    private ArrayList<Group> groups;
+    private List<Group> groups;
 
     public User() {}
 
     public User(String userName, String email) {
         this.userName = userName;
         this.email = email;
+        this.groups = new ArrayList<>();
     }
 
     public void lookUpUser(String email) {
     }
 
-    public Group createNewGroup(String groupName, User admin) {
+    public Group createNewGroup(String groupName) {
         ArrayList<User> members = new ArrayList<User>();
-        members.add(admin);
-        Group newGroup = new Group(groupName, admin, members, null);
+        members.add(this);
+        Group newGroup = new Group(groupName, this, members, null);
         newGroup.name = groupName;
-        admin.groups.add(newGroup);
+        groups.add(newGroup);
         return newGroup;
     }
 
     public String getUserName() {
         return this.userName;
-    }
-
-    public boolean deleteGroup(String groupName) {
-        for(int i = 0; i < this.groups.size(); i++) {
-            if(this.groups.get(i).name.equals(groupName)) {
-                this.groups.remove(i);
-                return true;
-            }
-        }
-        //Throw an exception here or somehow display to user that they aren't in a group with that name
-        return false;
     }
 
     public boolean leaveGroup(String groupName) {
@@ -57,7 +48,7 @@ public class User {
 
     }
 
-    public ArrayList<Group> getGroups () {
+    public List<Group> getGroups () {
         return groups;
     }
 
