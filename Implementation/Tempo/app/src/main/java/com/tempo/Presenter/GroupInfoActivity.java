d53ac67105f93393fb4898ced2d2d5a34f54ef11
Andrew Cofano <acofano@calpoly.edu>
2017-03-11 15:23:01 -0800

    #644392 Create activities to make group list and group info page

---
+++ Implementation/Tempo/app/src/main/java/com/tempo/Presenter/GroupInfoActivity.java
@@ -0,0 +1,14 @@
+package com.tempo.Presenter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class GroupInfoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_info);
+    }
+}
--- Implementation/Tempo/app/src/main/java/com/tempo/Presenter/MyCalendarActivity.java
+++ Implementation/Tempo/app/src/main/java/com/tempo/Presenter/MyCalendarActivity.java
@@ -1,19 +1,26 @@
 package com.tempo.Presenter;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.CalendarView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.alamkanak.weekview.MonthLoader;
-import com.alamkanak.weekview.WeekView;
+import android.graphics.PorterDuff.Mode;
 
-import com.alamkanak.weekview.WeekViewEvent;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.api.client.http.HttpTransport;
@@ -31,7 +38,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.tempo.Model.CalendarEvent;
+import com.tempo.Model.Group;
 import com.tempo.Model.User;
+
+import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -41,8 +51,13 @@ import java.util.List;
 
 public class MyCalendarActivity extends Activity {
 
+
     public enum CalendarType {
         MONTH, WEEK, DAY
+    }
+
+    public enum TabType {
+        HOME, GROUPS, SETTINGS
     }
 
     public class MyDate {
@@ -59,29 +74,39 @@ public class MyCalendarActivity extends Activity {
 
     private CalendarManager manager;
     private ViewGroup rootView;
-    private View monthView, weekView, dayView, currentView;
-    private ListView dayEventsList;
+    private ViewGroup tabRootView;
+    private View monthView, weekView, dayView, currentView, currentTabView, homeView, groupsView;
+    private ListView dayEventsList, groupListView;
     private EventListAdapter eventListAdapter;
+    private GroupListAdapter groupListAdapter;
     private CalendarType currentCalendar;
+    private TabType currentTab;
     private CalendarView monthlyCalendar;
     private WeekView weeklyCalendar;
+    private TextView dayDateText;
+
+
 
     private EventDateTime currentDate = null;
     private long dateInMillis;
     private String dateString;
 
     private ArrayList<CalendarEvent> currentDayEventList;
+    private ArrayList<Group> groupList;
 
-
+    private static Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        MyCalendarActivity.context = getApplicationContext();
         setContentView(R.layout.activity_my_calendar);
 
+        renderTabs();
         renderCalendar();
         displayEvents();
+        setTabTransitions();
         setCalendarTransitions();
 
         //new SyncCalendarTask(Account.getInstance().googleCred).execute();
@@ -89,25 +114,47 @@ public class MyCalendarActivity extends Activity {
 
     }
 
+
+    public static Context getAppContext() {
+        return MyCalendarActivity.context;
+    }
+
+    private void renderTabs() {
+        LayoutInflater inflater = getLayoutInflater();
+
+        rootView  = (ViewGroup) findViewById(R.id.tabRoot);
+        homeView = inflater.inflate(R.layout.home_tab, null);
+        currentTabView = homeView;
+        currentTab = TabType.HOME;
+        rootView.addView(homeView);
+
+        //monthlyCalendar = (CalendarView) findViewById(R.id.monthlyCalendar);
+
+
+        groupsView   = inflater.inflate(R.layout.group_tab, null);
+
+    }
+
     private void renderCalendar() {
 
         LayoutInflater inflater = getLayoutInflater();
 
-        rootView  = (ViewGroup) findViewById(R.id.calendarRoot);
+        //rootView  = (ViewGroup) findViewById(R.id.tabRoot);
+        tabRootView = (ViewGroup) findViewById(R.id.homeRoot);
         monthView = inflater.inflate(R.layout.calendar_month, null);
         currentView = monthView;
         currentCalendar = CalendarType.MONTH;
-        rootView.addView(monthView);
+        tabRootView.addView(monthView);
 
         monthlyCalendar = (CalendarView) findViewById(R.id.monthlyCalendar);
         dateInMillis = monthlyCalendar.getDate();
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
         dateString = formatter.format(new Date(dateInMillis));
         monthlyCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView calendarView, int year, int month, int day) {
-                Toast.makeText(getApplicationContext(), day + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
-                setDateString(day + "/" + month + "/" + year);
+                Toast.makeText(getApplicationContext(), (month + 1) + "/" + day + "/" + year, Toast.LENGTH_SHORT).show();
+                setDateString((month + 1) + "/" + day + "/" + year);
             }
         });
 
@@ -119,13 +166,101 @@ public class MyCalendarActivity extends Activity {
         dateString = newDate;
     }
 
-    private List<WeekViewEvent> getEvents(int newYear, int newMonth) {
-        List<WeekViewEvent> list = new ArrayList<>();
-        list.add(new WeekViewEvent());
-        return list;
-    }
 
     private void displayEvents() {
+
+    }
+
+
+    private void setTabTransitions() {
+        // Set listener for switching to home tab
+        findViewById(R.id.home).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyCalendarActivity.this.setCurrentTab(TabType.HOME);
+                Resources res = getAppContext().getResources();
+                final ImageView image = (ImageView) findViewById(R.id.home);
+                final int color = res.getColor(R.color.currentTabWhite);
+                image.setColorFilter(color, Mode.SRC_ATOP);
+
+            }
+        });
+
+        // Set listener for switching to group tab
+        findViewById(R.id.groups).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyCalendarActivity.this.setCurrentTab(TabType.GROUPS);
+                Resources res = getAppContext().getResources();
+                final ImageButton image = (ImageButton) findViewById(R.id.home);
+                //final int color = res.getColor(R.color.currentTabWhite);
+                image.setColorFilter(Color.argb(255, 255, 255, 255));
+            }
+        });
+    }
+
+
+    public void setCurrentTab(TabType current) {
+
+        ArrayList<User> tempArray;
+
+        if (currentTab == current) {
+            return;
+        }
+
+        rootView.removeView(currentTabView);
+
+        switch (current) {
+
+            case HOME:
+                rootView.addView(homeView);
+                currentTabView = homeView;
+                currentTab = TabType.HOME;
+                break;
+
+            // case WEEK:
+                /*rootView.addView(weekView);
+                currentView = weekView;
+                currentCalendar = CalendarType.WEEK;
+                break;*/
+
+            case GROUPS:
+                rootView.addView(groupsView);
+                currentTabView = groupsView;
+                currentTab = TabType.GROUPS;
+
+
+                groupListView = (ListView) findViewById(R.id.groupList);
+                groupList = new ArrayList<>();
+                tempArray = new ArrayList<>();
+                tempArray.add(new User("Alex BOId", "alex@teamlead.com"));
+                tempArray.add(new User("Alex BOId", "alex@teamlead.com"));
+                tempArray.add(new User("Alex BOId", "alex@teamlead.com"));
+                tempArray.add(new User("Alex BOId", "alex@teamlead.com"));
+                groupList.add(new Group("Falessi's Italianos", new User("Alex BOId", "alex@teamlead.com"), tempArray));
+                groupList.add(new Group("Other team", new User("Alex BOId", "alex@teamlead.com"), tempArray));
+                groupList.add(new Group("Other other team", new User("Alex BOId", "alex@teamlead.com"), tempArray));
+
+
+                groupListAdapter = new GroupListAdapter(this, groupList);
+                groupListView.setAdapter(groupListAdapter);
+
+                groupListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
+                        Group selectedGroup = groupList.get(position);
+                        startGroupInfoActivity(selectedGroup);
+
+                    }
+                });
+
+
+        }
+    }
+
+    private void startGroupInfoActivity(Group selectedGroup) {
+        Intent i = new Intent(this, GroupInfoActivity.class);
+        startActivity(i);
 
     }
 
@@ -163,12 +298,12 @@ public class MyCalendarActivity extends Activity {
             return;
         }
 
-        rootView.removeView(currentView);
+        tabRootView.removeView(currentView);
 
         switch (calendar) {
 
             case MONTH:
-                rootView.addView(monthView);
+                tabRootView.addView(monthView);
                 currentView = monthView;
                 currentCalendar = CalendarType.MONTH;
                 break;
@@ -180,8 +315,11 @@ public class MyCalendarActivity extends Activity {
                 break;*/
 
             case DAY:
-                rootView.addView(dayView);
+                tabRootView.addView(dayView);
                 currentView = dayView;
+
+                dayDateText = (TextView) findViewById(R.id.dayDate);
+                dayDateText.setText(dateString);
                 dayEventsList = (ListView) findViewById(R.id.eventList);
                 currentDayEventList = new ArrayList<>();
                 currentDayEventList.add(new CalendarEvent("Test 1", "Testing", "The Den", new EventDateTime(), new EventDateTime(), new ArrayList<User>(), new EventDateTime()));
